0b96a4f15f40a23e22c4f516e29b51cbcde1ed10
viktorbeznosov <viktorbeznosov@mail.ru>
2021-01-20 14:19:34 +0300

    migrations

---
+++ database/migrations/2021_01_20_111643_create_client_numbers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientNumbersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_numbers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('client_id')->unsigned();
+            $table->string('number', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_numbers');
+    }
+}
